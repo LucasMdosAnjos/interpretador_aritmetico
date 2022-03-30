9867c2cab0266fe17e5a2fe5d6c6014976355303
Kevin Littles <pro.kevinribeiro@gmail.com>
2022-03-29 21:48:01 -0300

    Init repo

---
+++ Interpretador Aritmético/Interpretador Aritmético/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Interpretador Aritmético
+//
+//  Created by Kevin Littles on 29/03/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
