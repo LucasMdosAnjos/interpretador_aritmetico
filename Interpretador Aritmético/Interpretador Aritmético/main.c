3f5cdac5e8c98c46023366d6db936e68dbda168b
LucasMdosAnjos <lucasmon@live.com>
2022-04-05 22:24:43 -0300

    ajustes

--- Interpretador Aritmético/Interpretador Aritmético/main.c
+++ Interpretador Aritmético/Interpretador Aritmético/main.c
@@ -6,7 +6,7 @@ int main(void)
 
     char url[] = "../RPN.txt";
     char ch;
-    char texto_str[4];
+    char texto_str[5];
     FILE *pont_arq;
 
     pont_arq = fopen(url, "r");
@@ -16,16 +16,42 @@ int main(void)
     }
     else
     {
-        while (fgets(texto_str, 4, pont_arq) != NULL){
-           // printf("%s",texto_str);
+        while (fgets(texto_str, 5, pont_arq) != NULL)
+        {
+            // printf("%s",texto_str);
             int tamanho = strlen(texto_str);
             int i = 0;
-            for( i= 0; i < tamanho ; i++){
-                //Lógica
-                printf("%c",texto_str[i]);
+
+            for (i = 0; i < tamanho; i++)
+            {
+                switch (texto_str[i])
+                {
+                case '+':
+                    printf("soma\n");
+                    break;
+                case '*':
+                    if (i + 1 < tamanho && texto_str[i + 1] == '*')
+                    {
+                        printf("POT\n");
+                    }
+                    else
+                    {
+                        if (texto_str[i - 1] != '*')
+                        {
+                            printf("multi\n");
+                        }
+                    }
+                    break;
+                case '-':
+                    printf("sub\n");
+                    break;
+                case '/':
+                    printf("div\n");
+                    break;
+                }
+                // Lógica
             }
         }
-        
     }
 
     fclose(pont_arq);
